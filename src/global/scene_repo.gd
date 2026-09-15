d28d5163bf6266b76ba425874980e0ad0157ee50
Steven Smith <stevenoliverssmith@gmail.com>
2026-09-15 15:55:05 -0400

    Started on a real Level One

--- src/global/scene_repo.gd
+++ src/global/scene_repo.gd
@@ -5,3 +5,9 @@ const FRAMEWORK: Dictionary = {
 	"main_playspace": "uid://dxuqr432d70id",
 	"loading_screen": "uid://cptmy52bg5lks"
 }
+
+const LEVELS: Dictionary = {
+	"test": "uid://urfuelmsqpwo",
+	"trenchbroom_prototype": "uid://b6a11iihi378e",
+	"level_one": "uid://bhl1d87hwrwc2"
+}
--- src/scenes/main_playspace/managers/game_manager.gd
+++ src/scenes/main_playspace/managers/game_manager.gd
@@ -2,6 +2,7 @@ class_name GameManager extends Node
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("dev_exit"):
-		SceneLoader.load_scene(SceneRepo.FRAMEWORK.menu)
+		#SceneLoader.load_scene(SceneRepo.FRAMEWORK.menu)
+		get_tree().quit()
 	if event.is_action_pressed("dev_reload"):
 		get_tree().reload_current_scene()
